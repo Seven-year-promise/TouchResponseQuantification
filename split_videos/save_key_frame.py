85462ea8b8807d9e3f5142985ba3463aff61b80f
yankee.wann@gmail.com <zw100115105425>
2021-03-29 15:05:12 +0200

    save images for second paper (real-time image processing) 0329

--- split_videos/save_key_frame.py
+++ split_videos/save_key_frame.py
@@ -12,29 +12,21 @@ SAVE_Y_MIN = 100
 SAVE_Y_MAX = 380
 
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--video_path', type=str, default = './pipeline_multi-larva-touching/WT_150558_Speed25.avi',
-                   help='sum the integers (default: find the max)')
-parser.add_argument('--save_path', type=str, default = './pipeline_multi-larva-touching/cropped/WT_150558_Speed25.avi',
+parser.add_argument('--video_path', type=str, default = './pipeline_multi-larva-touching/WT_144617_Speed25.avi',
                    help='sum the integers (default: find the max)')
 args = parser.parse_args()
 
 if __name__ == '__main__':
     cap = cv2.VideoCapture(args.video_path)
-    #fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(args.save_path, cv2.VideoWriter_fourcc('M','J','P','G'), 20.0, (SAVE_Y_MAX-SAVE_Y_MIN+1, SAVE_X_MAX-SAVE_X_MIN+1))
 
     fame_id = 0
     success, frame = cap.read()  # "/home/ws/er3973/Desktop/research_code/TailTouching.avi"
-
+    frames = []
+    frames.append(frame)
     while success:
-        fame_id += 1
-        if fame_id%20 == 0:
-            #frame = cv2.flip(frame, 0)
-
-            out.write(frame[SAVE_Y_MIN:SAVE_Y_MAX, SAVE_X_MIN:SAVE_X_MAX])
+        frames.append(frame[SAVE_Y_MIN:SAVE_Y_MAX, SAVE_X_MIN:SAVE_X_MAX])
 
         success, frame = cap.read()
 
 
-    cap.release()
-    out.release()
+    cv2.imwrite("./key_frame/last_frame.png", frames[-1])
